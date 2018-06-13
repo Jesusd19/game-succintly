a5e85567b4e5dda5f8cc32baa3c5192f423bd268
Ngenko <jesusdbarajas@gmail.com>
2018-06-13 16:31:31 -0500

    generando las tablas

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Game from './componet/Game';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Game rows={5} columns={6} />       
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/componet/Cell.js
@@ -0,0 +1,9 @@
+import React from 'react';
+export default class Cell extends React.Component {
+    render() {
+        return (
+            <div className="cell">
+                {this.props.id}
+            </div>);
+    }
+}
